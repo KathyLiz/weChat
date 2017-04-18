bcf5482ef4d14e298bc30de1e04739517fef2506
Katherine Hurtado <kathyta45@hotmail.com>
2017-04-18 11:30:39 -0500

    Ser han corregido errores de sintaxis

--- config/express.js
+++ config/express.js
@@ -1,5 +1,5 @@
 var express = require ('express');
-var bodyParser = requiere('body-parser');
+var bodyParser = require('body-parser');
 var favicon = require('static-favicon');
 var lessMiddleware = require ('less-middleware');
 var pkg = require ('../package');
@@ -8,12 +8,12 @@ var xmlparser = require('express-xml-bodyparser');
 
 module.exports = function (app,config){
 app.set('showStackError',true);
-app.use(favicon(config.root{'/public/images/favicon.ico'));
+//app.use(favicon(config.root{'/public/images/favicon.ico'));
 app.use(bodyParser());
 app.locals.pretty = true;
 
 app.use(xmlparser());
-app.use(express.estatic(config.root+'/public'));
+//app.use(express.estatic(config.root+'/public'));
 
 app.use(function(req,res,next){
 res.locals.pkg = pkg;
